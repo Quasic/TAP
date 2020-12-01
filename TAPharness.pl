2fd64b73e7265eb61ab7bd37a2c534b3b6cc398b
Quasic <qbasic010@aim.com>
2020-12-01 02:12:54 -0500

    TAPharness.pl: fix skipping

--- TAPharness.pl
+++ TAPharness.pl
@@ -11,29 +11,29 @@ exit 1 if ((TAP::Harness->new({
     if($f=~/\.ps1$/){
       $haspowershell=&which('powershell.exe')if!defined$haspowershell;
       return[$haspowershell,$f]if$haspowershell;
-      return"#TAP testing $f\n1..0 - #Skipped: no PowerShell\n";
+      return"#TAP testing $f\n1..0 #Skipped: no PowerShell\n";
     }
     if($f=~/\.(bat|cmd|nt)$/){
       $hasdos=&which(qw(cmd.exe command.com))if!defined$hasdos;
       return[$hasdos,'//c',$f]if$hasdos;
-      return"#TAP testing $f\n1..0 - #Skipped: no DOS\n";
+      return"#TAP testing $f\n1..0 #Skipped: no DOS\n";
     }
     if($f=~/\.bas$/){
       $hasbasic=&which(qw(qbasic basic))if!defined$hasbasic;
       return[$hasbasic,$f]if$hasbasic;
-      return"#TAP testing $f\n1..0 - #Skipped: no BASIC\n";
+      return"#TAP testing $f\n1..0 #Skipped: no BASIC\n";
     }
     if($f=~/\.(vbs|wsf)$/){
       $hascscript=&which('cscript.exe')if!defined$hascscript;
       return['cscript','//nologo',$f]if$hascscript;
-      return"#TAP testing $f\n1..0 - #Skipped: no cscript\n";
+      return"#TAP testing $f\n1..0 #Skipped: no cscript\n";
     }
     if($f=~/\.js$/){
       $hasnode=&which('node')if!defined$hasnode;
       return['node',$f]if$hasnode;
       $hascscript=&which('cscript.exe')if!defined$hascscript;
       return['cscript','//nologo',$f]if$hascscript;
-      return"#TAP testing $f\n1..0 - #Skipped: no node or cscript\n";
+      return"#TAP testing $f\n1..0 #Skipped: no node or cscript\n";
     }
     #lua, python
     open my$h,"<$f"or return"#TAP testing $f\n1..1\nnot ok - failed to open $f\n";
