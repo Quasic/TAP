563329cf3a63888f2eeaa61c52212670a7bb0ae1
Quasic <qbasic010@aim.com>
2020-12-31 01:22:38 -0500

    lintTAPgit<0.3> header&version; mv lintTAPsh->lintTAPbash

--- TAP/lintTAPgit.sh
+++ TAP/lintTAPgit.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#runs checks on the git repository
-#based on default git pre-commit hook
+# runs checks on the git repository
+# based on default git pre-commit hook
+# By Quasic
+# Report bugs to https://github.com/Quasic/TAP/issues
+# Released under Creative Commons Attribution (BY) 4.0 license
+Version=0.3
 if git rev-parse --verify HEAD >/dev/null 2>&1
-then
-        against=HEAD
+then against=HEAD
 else
         # Initial commit: diff against an empty tree object
 	if ! against=$(git hash-object -w -t tree /dev/null)
@@ -14,7 +17,7 @@ else
 	fi
 fi
 #shellcheck source=TAP/TAP.sh
-source "$(dirname "${BASH_SOURCE[0]}")/TAP.sh" "$(basename "$0")" 5
+source "$(dirname "${BASH_SOURCE[0]}")/TAP.sh" "$(basename "${BASH_SOURCE[0]}") $Version" 5
 [ "$(git config --bool hooks.allownonascii)" == 'true' ]&&skip 'hooks.allownonascii is true' 2
 is "$(git diff --name-only --cached --diff-filter=A -z "$against" | LC_ALL=C tr -d '[ -~]\0' | wc -c)" 0 'staged ASCII filenames'
 is "$(git diff --name-only --diff-filter=A -z "$against" | LC_ALL=C tr -d '[ -~]\0' | wc -c)" 0 'ASCII filenames'
