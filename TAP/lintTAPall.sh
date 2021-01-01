0f927e8d3bd31408f46600846d69801dea236c32
Quasic <qbasic010@aim.com>
2020-12-31 20:41:12 -0500

    lintTAPall<0.2> version, header, source instead of subshell

--- TAP/lintTAPall.sh
+++ TAP/lintTAPall.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# gives lint.sh all files/subdirectories recursively
+# By Quasic
+# Report bugs to https://github.com/Quasic/TAP/issues
+# Released under Creative Commons Attribution (BY) 4.0 license
+printf '%s\n' "${BASH_SOURCE[0]} 0.2"
 shopt -s nullglob globstar
-bash "$(dirname "${BASH_SOURCE[0]}")/lint.sh" ./**/*
+#shellcheck source=TAP/lint.sh
+source "$(dirname "${BASH_SOURCE[0]}")/lint.sh" ./**/*
