da7e56f6d21f68dd3b15b20ed07231fc2c0c7df6
Quasic <qbasic010@aim.com>
2021-01-08 22:47:25 -0500

    lint.sh<1.0e> reduce version header by reporting on each test, handle no args

--- TAP/lint.sh
+++ TAP/lint.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
-printf '#TAP lint.sh 1.0d'
+printf '#TAP lint.sh 1.0e'
 [ "$1" = '--version' ]&&printf '\n'&&exit
 dir=$(dirname "${BASH_SOURCE[0]}")
 declare -A has
-printf '; %s' "$(bash --version|head -n1)"
-command -v gawk>/dev/null&&
-	y=$(gawk -v help=version -f "$dir/lintparse.awk")&&
-	has[gawk]="$y"&&
-	printf '; %s' "$y"
-command -v make>/dev/null&&
-	y=$(make --version)&&
-	has[make]="$y"&&
-	awk '{sub(/,.*$/,"");printf "; %s",$0;exit}'<<<"$y"
-command -v cscript.exe>/dev/null&&
-	y=$(cscript.exe //Nologo //Job:version "$dir/lint.wsf")&&
-	has[CScript]="$y"&&
-	printf '; %s' "$y"
-command -v shellcheck>/dev/null&&
-	y=$(shellcheck --version)&&
-	has[shellcheck]="$y"&&
-	awk '/^version:/{printf "; shellcheck %s",$2;exit}'
-command -v node>/dev/null&&
-	y=$(node --version 2>/dev/null)&&
-	has[node]="$y"&&
-	printf '; node %s' "$y" #node alias w/o node in Msys
-command -v gcc>/dev/null&&
-	y=$(gcc -dumpversion 2>/dev/null)&&
-	has[gcc]="$y"&&
-	printf '; gcc %s' "$y ($(gcc -dumpmachine))"
+if command -v bash>/dev/null
+then
+	printf '; %s' "$(bash --version|head -n1)"
+	has[bash]=1
+else printf '; Unknown shell'
+fi
+if command -v awk>/dev/null
+then
+	has[awk]='awk'
+	y='found'
+elif command -v gawk>/dev/null
+then
+	has[awk]='gawk'
+	alias awk=gawk
+	y=$(gawk -v help=version -f "$dir/lintparse.awk")&&has[gawk]="$y"
+elif command -v mawk>/dev/null&&y="$(mawk --version|head -n1)"
+then
+	has[awk]='mawk'
+	alias awk=mawk
+	has[mawk]="$y"
+else y='not found'
+fi
+printf '; awk: %s' "(${has[awk]}) $y"
+hasCScript(){
+	local t
+	if [ ${has[CScript]+set} ]
+	then [ "${has[CScript]}" = '' ]&&return 1
+	else
+		if command -v cscript.exe>/dev/null
+		then
+			has[CScript]=1
+			t=$(cscript.exe //Nologo //Job:version "$dir/lint.wsf")&&has[lint.wsf]="$t"
+		else has[CScript]=''
+		fi
+	fi
+}
+hasnode(){
+	local t
+	if [ ${has[node]+set} ]
+	then [ "${has[node]}" = '' ]&&return 1
+	else
+		if command -v node>/dev/null&&
+			t=$(node --version 2>/dev/null)
+		then has[node]="$t";return 0
+		else has[node]='';return 1
+		fi
+	fi
+}
 printf '\n'
 [ "$1" = '--versions' ]&&exit
 if [ "$1" = '--help' ]||[ "$1" = '-h' ]
@@ -45,6 +68,10 @@ Please report bugs at https://github.com/Quasic/TAP/issues
 Released under Creative Commons Attribution (BY) 4.0 license
 ' "$0"
 	[[ $- == *i* ]]&&read -rn1 -p "Press a key to close help">&2
+	exit
+elif [ $# = 0 ]
+then
+	printf '# use lint.sh --help or -h option for help\n1..0 # skipped without tests'
 	exit
 fi
 if [ "$1" = '-p' ]
@@ -84,7 +111,8 @@ do
 		'<!--')y=wsf;;
 		*)y='?';;
 		esac
-	elif [[ "$f" =~ \.(g?awk|bash|dash|hta|js|ksh|sed|sh|vbs|ws[cf]|xhtml)$ ]]
+	elif [[ "$f" =~ \.(g?awk|bash|dash|hta|js|ksh|sed|sh|vbs|ws[cf]|xhtml|xht)$ ]]
+	#aspx? html?
 	then y="${BASH_REMATCH[1]}"
 	elif [[ "$f" =~ \.(b|d)sh$ ]]
 	then y="${BASH_REMATCH[1]}ash"
@@ -93,44 +121,103 @@ do
 	elif [[ "$f" =~ \.[ch]$ ]]
 	then y=c
 	elif [ "$f" = Makefile ]
-	then y=make
+	then y='make'
 	else y='?'
 	fi
 	if [[ "$y" =~ ^(bash|dash|ksh|sh)$ ]]
 	then
+		[ ${has["$y"]+set} ]||has["$y"]=$(command -v "$y")
+		[ "${has["$y"]}" = '' ]&&skip "$y not found" 1
 		okname "$f lint $y" "$y" -n "$f"
-		[ "${has[shellcheck]}" != '' ]&&okname "$f shellcheck" shellcheck -x "$f"
+		if ! [ ${has[shellcheck]+set} ]
+		then
+			if command -v shellcheck>/dev/null&&
+				y=$(shellcheck --version)
+			then
+				if [ "${has[awk]}" = '' ]
+				then has[shellcheck]=$(grep ^version:<<<"$y")
+				else has[shellcheck]=$(awk '/^version:/{print $2;exit}'<<<"$y")
+				fi
+			else has[shellcheck]=''
+			fi
+		fi
+		[ "${has[shellcheck]}" != '' ]&&okname "$f shellcheck ${has[shellcheck]}" shellcheck -x "$f"
 	elif [ "$y" = perl ]
 	then okname "$f lint Perl" perl -wct "$f"
-	elif [[ "$y" =~ ^g?awk$ ]]&&
-		[ "${has[gawk]}" != '' ]
-	then gawk --lint -e 'BEGIN{exit 0}END{exit 0}' -f "$f" 2>&1|gawk -v file="$f" -v dialect="$y" -v shebang="$shebang" -f "$dir/lintparse.awk"||((TAP_TestsFailed++))
+	elif [[ "$y" =~ ^g?awk$ ]]
+	then
+		[ "$y" = awk ]&&okname "$f lint awk by immediate exit" "${has[awk]}" -e 'BEGIN{exit 0}END{exit 0}' -f "$f"
+		if ! [ ${has[gawk]+set} ]
+		then
+			if command -v gawk>/dev/null&&
+				y=$(gawk -v help=version -f "$dir/lintparse.awk")
+			then has[gawk]=1
+			else has[gawk]=''
+			fi
+		fi
+		if [ "${has[gawk]}" != '' ]
+	then
+		#shellcheck disable=SC2031
+		gawk --lint -e 'BEGIN{exit 0}END{exit 0}' -f "$f" 2>&1|gawk -v file="$f" -v dialect="$y" -v shebang="$shebang" -f "$dir/lintparse.awk"||((TAP_TestsFailed++))
+		#shellcheck disable=SC2031
 		((TAP_TestsRun++))
+		fi
 	# elif [ "$y" = sed ]
 	# then
-	elif [ "$y" = make ]&&
-		[ "${has[make]}" != '' ]
-	then okname "$f lint make" make -n
-	elif [ "$y" = node ]&&
-		[ "${has[node]}" != '' ]
-	then okname "$f lint node.js" node -c "$f"
-	elif [[ "$y" = js ]]&&
-		[ "${has[CScript]}${has[node]}" != '' ]
+	elif [ "$y" = make ]
 	then
-		[ "${has[CScript]}" != '' ]&&
-			okname "$f lint cscript.exe" cscript.exe //Nologo //Job:lint "$dir/lint.wsf" "$f"
-		[ "${has[node]}" != '' ]&&
-			okname "$f lint node.js" node -c "$f"
-	elif [[ "$y" =~ ^(vbs|wsf|wsc|hta|xhtml)$ ]]&&
-		[ "${has[CScript]}" != '' ]
-	then okname "$f lint cscript.exe" cscript.exe //Nologo //Job:lint "$dir/lint.wsf" "$f"
-	elif [ "$y" = c ]&&[ "${has[gcc]}" != '' ]
+		if ! [ ${has[make]+set} ]
+		then
+			if command -v make>/dev/null&&
+				y=$(make --version)
+			then
+				if [ "${has[awk]}" = '' ]
+				then has[make]=$(head -n1<<<"$y")
+				else has[make]=$(awk '{sub(/,.*$/,"");print;exit}'<<<"$y")
+				fi
+			else has[make]=''
+			fi
+		fi
+		[ "${has[make]}" = '' ]&&skip 'no make found' 1
+		okname "$f lint make ${has[make]}" make -n
+	elif [ "$y" = node ]&&hasnode
+	then okname "$f lint node.js ${has[node]}" node -c "$f"
+	elif [[ "$y" = js ]]
 	then
-		okname "preprocess $f" gcc -E "$f" -o /dev/null
+		hasCScript&&[ "${has[lint.wsf]}" != '' ]&&okname "$f lint cscript.exe ${has[lint.wsf]}" cscript.exe //Nologo //Job:lint "$dir/lint.wsf" "$f"
+		hasnode&&okname "$f lint node.js ${has[node]}" node -c "$f"
+		if [ "${has[node]}${has[lint.wsf]}" = '' ]
+		then
+			skip "$f unable to lint JavaScript" 1
+			fail 'no lint'
+		fi
+	elif [[ "$y" =~ ^(vbs|wsf|wsc|hta|xhtml)$ ]]&&hasCScript
+	then [ "${has[lint.wsf]}" != '' ]&&okname "$f lint cscript.exe ${has[lint.wsf]}" cscript.exe //Nologo //Job:lint "$dir/lint.wsf" "$f"
+		if [ "$y" = wsf ]
+		then
+			F='lint_wsf_unjob_'
+			b=0
+			while grep -F "$F$b" "$f"
+			do ((b++))
+			done
+			is "$(cscript.exe //nologo "//job:$F$b" "$f")" "Input Error: Unable to find job \"$F$b\"." 'lint wsf unjob test'
+		fi
+	elif [ "$y" = c ]
+	then
+		if ! [ ${has[gcc]+set} ]
+		then
+			if command -v gcc>/dev/null&&
+				y=$(gcc -dumpversion 2>/dev/null)
+			then has[gcc]="$y ($(gcc -dumpmachine))"
+			else has[gcc]=''
+			fi
+		fi
+		[ "${has[gcc]}" = '' ]&&skip 'no gcc found' 3
+		okname "preprocess $f gcc ${has[gcc]}" gcc -E "$f" -o /dev/null
 		okname "compile $f" gcc -S "$f" -o /dev/null
 		okname "assemble $f" gcc -c "$f" -o /dev/null
 	else
-		skip "$f Unknown lint type $y shebang: $shebang" 1
+		skip "$f unable to lint type $y shebang: $shebang" 1
 		fail 'no lint'
 	fi
 done
